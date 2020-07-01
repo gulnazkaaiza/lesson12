185182de35c612c5f13e99fb1562b9f5052e4fe5
Gulnaz Aizatullina <gulnazka.aiza@gmail.com>
2020-07-01 15:02:06 +0300

    Написала 3 задачи!)

--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -6,6 +6,12 @@ public class Main {
         Cooking anna = new Cooking("Anna");
         anna.haveIngredients();
         anna.haveSaucePan();
-        anna.cook();
+        try {
+            anna.cook();
+        } catch (NotReadyToStartCookingException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Проверяем достают ли все составляющие. Есть ли кастрюля?  " + anna.sausePan + "\r\n Имеются ли все ингредиенты? "
+                    + anna.ingredients + "\r\n Включен ли газ? " + anna.gasOn);
+        }
+        }
     }
-}
